55a1df8a1386a831a5daff1e3704bd9a25d0a10f
danridepdanri <106473655+danridepdanri@users.noreply.github.com>
2022-10-02 21:34:18 +0300

    Add files via upload

---
+++ Myexecrefslib/Myexecrefslib.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include "../FractionLib/Fraction.h"
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "ru");    //русский язык
+
+
+	Fraction N; // инициализация двух частей через статические данные конструктора
+	N.display();
+	Fraction M(25, 56); // инициализация через одно значение, второе будет дефолтное 
+	M.display();
+	Fraction S(66, 44); // типичная инициализация с двумя параметрами 
+	S.display();
+	
+	system("pause");
+
+
+
+	return 0;
+}
